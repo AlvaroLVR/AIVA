e21d41103fb0b9a9c0b7ec37f0d598ff8386136d
alvaro valencia <alvaroleandro74@gmail.com>
2022-09-26 15:54:45 -0300

    agregando SASSÂ

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import {NavBar} from './components/navBar'
 
 function App() {
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,47 +1,45 @@
-
 export const NavBar = () =>{
-    return <div>
+    return <>
         <div className="header container-fluid navbar sticky-top bg-dark navbar-expand-lg ">
-        <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-        </button>
+            <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
         
-        <a className="navbar-brand header-logo" href="#">
-            MARCA
-        </a>
+            <a className="navbar-brand header-logo" href="#">
+                MARCA
+            </a>
         
-        <div  className="collapse navbar-collapse header-lista" id="navbarTogglerDemo03">
-            
-            <ul id="header-list-left" className="navbar-nav  mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Hombres</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Mujeres</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Accesorios</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Importados</a>
-                </li>
-            </ul>
-            
-            <ul id="header-list-right" className="navbar-nav  mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Sobre nosotros</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">Blog</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light fs-6" href="#">
-                            carrito
-                    </a>
-                </li>
-            </ul>
+            <div  className="collapse navbar-collapse header-lista" id="navbarTogglerDemo03">
+                
+                <ul id="header-list-left" className="navbar-nav  mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Hombres</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Mujeres</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Accesorios</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Importados</a>
+                    </li>
+                </ul>
+                
+                <ul id="header-list-right" className="navbar-nav  mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Sobre nosotros</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">Blog</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light fs-6" href="#">
+                                carrito
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-
-    </div>
+    </>
 };
