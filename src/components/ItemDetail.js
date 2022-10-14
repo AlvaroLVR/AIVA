789d12eab57bc225ec374b94d6c8bd12bade2e4b
alvaro valencia <alvaroleandro74@gmail.com>
2022-10-14 14:46:15 -0300

    segunda pre-entrega terminada

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -8,17 +8,17 @@ export default function ItemDetail(props) {
 
       <div className=' bg-light border-2  p-3 mt-4'>
         <div className='row'>
-          <div className='col-6  border-start border-primary d-flex justify-content-around'>
+          <div className='col-6  border-start border-dark d-flex justify-content-around'>
             {/* <img className='' src={props.imagen}/> */}
             <img className='d-flex align-center' style={{width: '25vw'}} src={props.prod.image} />
           </div>
-          <div className='col-6 border-start border-primary d-flex flex-column justify-content-between '>
+          <div className='col-6 border-start border-dark d-flex flex-column justify-content-between '>
             <h2 className='text-center'>{props.prod.title}</h2>
             <ul className=''>
-              <li className='list-group-item'> Categoria {props.prod.category}</li>
-              <li className='list-group-item fs-4 border rounded-2 p-2'>Descripcion: {props.prod.description}</li>
+              <li className='list-group-item'> <i> Categoria {props.prod.category}  </i> </li>
+              <li className='list-group-item fs-4 border rounded-1 p-2 bg-'>Descripcion: {props.prod.description}</li>
             </ul>
-            <span className='text-end fs-2'><b>Precio {props.prod.price} </b></span>
+            <span className='text-end fs-2'><b>Precio $ {props.prod.price}  </b></span>
           </div>
         </div>
       </div>
