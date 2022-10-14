59cc5b8c2eab598cb82b1d3816e6928d4eac8349
alvaro valencia <alvaroleandro74@gmail.com>
2022-10-14 14:51:02 -0300

    agregnado fondo al precio de ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -15,10 +15,10 @@ export default function ItemDetail(props) {
           <div className='col-6 border-start border-dark d-flex flex-column justify-content-between '>
             <h2 className='text-center'>{props.prod.title}</h2>
             <ul className=''>
-              <li className='list-group-item'> <i> Categoria {props.prod.category}  </i> </li>
+              <li className='list-group-item'>  Categoria <i> {props.prod.category}  </i> </li>
               <li className='list-group-item fs-4 '>Descripcion: {props.prod.description}</li>
             </ul>
-            <span className='text-end fs-2'><b>Precio $ {props.prod.price}  </b></span>
+            <div className='text-end fs-2 rounded-1 '><b className='bg-dark text-white p-2 rounded-1'>Precio $ {props.prod.price}  </b></div>
           </div>
         </div>
       </div>
