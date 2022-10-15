3aaaf33f0e5a56f0c5ae4e390cf3685742e64a3f
alvaro valencia <alvaroleandro74@gmail.com>
2022-10-14 21:21:14 -0300

    agregando Link a los items del navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logoMenu from '../img/logoMenu.svg'
 import CartWidget from './CartWidget';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () =>{
 
@@ -10,38 +11,38 @@ export const NavBar = () =>{
             <span className="navbar-toggler-icon"></span>
             </button>
         
-            <a className="navbar-brand header-logo" href="#">
+            <a className="navbar-brand header-logo" >
+                <Link to={'/'}>
                 <img className='svg' src={logoMenu} />
+                </Link> 
             </a>
         
             <div  className="collapse navbar-collapse header-lista " id="navbarTogglerDemo03">
                 
                 <ul id="header-list-left" className="navbar-nav mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Hombres</a>
+                        <Link to={'/categoria/hombres'} className="nav-link text-light fs-6" >Hombres</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Mujeres</a>
+                        <Link to={'/categoria/mujeres'} className="nav-link text-light fs-6" >Mujeres</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Accesorios</a>
+                        <Link to={'/categoria/accesorios'} className="nav-link text-light fs-6" >Accesorios</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Importados</a>
+                        <Link to={'/categoria/hombres'} className="nav-link text-light fs-6" >Importados</Link> 
                     </li>
                 </ul>
                 
                 <ul id="header-list-right" className="navbar-nav mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Sobre nosotros</a>
+                        <Link to={'/sobrenosotros'} className="nav-link text-light fs-6" >Sobre nosotros</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">Blog</a>
+                        <Link to={'/blog'} className="nav-link text-light fs-6" >Blog</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light fs-6" href="#">
-                            <CartWidget/>
-                        </a>
+                        <Link to={'/carrito'} className="nav-link text-light fs-6" > <CartWidget/> </Link>
                     </li>
                 </ul>
             </div>
