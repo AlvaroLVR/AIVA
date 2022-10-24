2a9feb1c29b45ccc93faea9144a86e7137642443
alvaro valencia <alvaroleandro74@gmail.com>
2022-10-24 14:46:24 -0300

    error con el setCarList

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,11 +5,13 @@ export const cartContext = createContext([])
 
 const CartContextProvider = ({children}) =>{
 
-    const [carList,setCartList] = useState({})
+    const [carList,setCartList] = useState([])
+    const [cantidad,setCartCantidad] = useState(0)
 
-    const addCart = (prod) =>{
+    const addCart = (prod,cant) =>{
         console.log(carList, 'esto recibe cartContext');
-        setCartList(prod)
+        cant<0 ? setCartCantidad(0) : setCartList([...prod,cant])
+        console.log(carList, 'esto se guarda');
     }
 
     return(
