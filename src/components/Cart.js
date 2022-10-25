59f1f912d4b7735038cbeee5a6465fa61e017a27
alvaro valencia <alvaroleandro74@gmail.com>
2022-10-25 14:55:08 -0300

    guardando ultimos cambios

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,14 +4,14 @@ import Item from './Item'
 
 export default function Cart() {
   const {carList,cantidad} = useContext(cartContext)
-  console.log(carList.prod,'estas en CART',cantidad);
+  console.log(carList,'estas en CART',cantidad);
 
   return (
     <div className='d-flex flex-column rounded-2 bg-light m-4'>
       <h1 className='text-center mt-4'>carrito</h1>
-      {
-        <Item key={carList.prod.id} datos={carList.prod}/>
-      }
+      {/* {
+        carList.map (prod => <Item key={prod.id} datos={prod}/>)
+      } */}
     </div>
   )
 }
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,12 +5,12 @@ export const cartContext = createContext([])
 
 const CartContextProvider = ({children}) =>{
 
-    const [carList,setCartList] = useState([{}])
+    const [carList,setCartList] = useState({})
     const [cantidad,setCartCantidad] = useState(0)
 
     function addCart (prod,cant){
         console.log(prod, 'esto recibe cartContext',cant);
-        setCartList({prod})
+        setCartList({...prod})
         setCartCantidad(cant)
     }
 
