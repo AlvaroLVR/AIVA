06d07d53e974328f8fb7e4baf08f8127f457d8e0
alvaro valencia <alvaroleandro74@gmail.com>
2022-11-23 07:08:24 -0300

    mayuscula en NavBar

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,9 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBq_6eoAjquJCXE28kT7qO2NvVzNE9rLCI",
   authDomain: "aiva-shop-6a65e.firebaseapp.com",
@@ -8,6 +12,8 @@ const firebaseConfig = {
   messagingSenderId: "477222047386",
   appId: "1:477222047386:web:4eab7d1fb261c9e112d923"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const firestoreInit = () =>{
