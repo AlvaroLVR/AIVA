f50ece5fc3cd80f71eaff9205cff5b1c0359d41c
alvaro valencia <alvaroleandro74@gmail.com>
2022-12-12 15:24:51 -0300

    agregando mayuscula a navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {NavBar} from './components/NavBar'
+import NavBar from './components/NavBar'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer'
